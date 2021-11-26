4789502ad0d8626b6e9c4e9fb15b1d9a2ffb171d
LukasMurdock <lukas.murdock@gmail.com>
2021-11-26 18:37:01 -0500

    Update 05-Lab.java

--- labs/.java/05-Lab.java
+++ labs/.java/05-Lab.java
@@ -1,58 +1,264 @@
+```java
+/**
+ * Lukas Murdock
+ * Assignment 4
+ */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
-public class Lab5 {
+public class Assignment4 {
+
+    private static boolean hasLoadedData = false;
+    private static boolean isSorted = false;
+    private static List<Customer> customers = new ArrayList<Customer>();
+    private static List<Customer> sortedCustomers = new ArrayList<Customer>();
+    private static Scanner keyboardReader = new Scanner(System.in);
+    private static String[] menuOptions = {
+        "Load from a file",
+        "Print the loaded list",
+        "Sort the list based on the nicknames",
+        "sort the list based on the ids",
+        "Print the sorted list",
+        "Exit"
+    };
 
     public static void main(String[] args) {
-        class Discount {
-            String name;
-            double[] discounts;
+        int menuSelection = -1;
+        while (menuSelection != 6) {
 
-            public Discount(String name, double[] discounts) {
-                this.name = name;
-                this.discounts = discounts;
+            menuSelection = menuSelection(menuOptions);
+
+            switch (menuSelection) {
+                case 1:
+                    loadCustomersFromFile();
+                    break;
+                case 2:
+                    printLoadedList();
+                    break;
+                case 3:
+                    sortCustomers("name");
+                    break;
+                case 4:
+                    sortCustomers("id");
+                    break;
+                case 5:
+                    printSortedList();
+                    break;
+                case 6:
+                    System.out.println("End!");
+                    break;
+                default:
+                    break;
             }
         }
-
-        Discount dogDiscount = new Discount("Dog discount", new double[]{0.10,
-            0.20});
-        Discount catDiscount = new Discount("Cat discount", new double[]{0.10,
-            0.15});
-        Discount rodentDiscount = new Discount("Rodent discount",
-            new double[]{0.10,
-                0.15});
-
-        Discount[] discounts = {dogDiscount, catDiscount, rodentDiscount};
-
-        Scanner keyboardReader = new Scanner(System.in);
-        int menuSelection = userInputMenuSelection(keyboardReader);
-
-        System.out.print("Enter total purchase: ");
-        double totalPurchase = keyboardReader.nextDouble();
-
-        int isOver100 = totalPurchase <= 100 ? 0 : 1;
-
-        String discountFormat = "%s (%f%%): %f";
-        double discountPercent =
-            (discounts[menuSelection - 1].discounts[isOver100] * 100);
-        double calculatedDiscount =
-            (totalPurchase * discounts[menuSelection - 1].discounts[isOver100]);
-
-        System.out.printf("%s (%.0f%%): %.2f\n",
-            discounts[menuSelection - 1].name,
-            discountPercent,
-            calculatedDiscount);
-        System.out.printf("Price Payable: %.2f\n",
-            totalPurchase - calculatedDiscount);
-        System.out.println("End");
     }
 
-    public static int userInputMenuSelection(Scanner keyboardReader) {
-        System.out.println("*Discount Calculator*");
-        System.out.println("1. Calculating dog discount");
-        System.out.println("2. Calculating cat discount");
-        System.out.println("3. Calculating rodent discount");
-        System.out.print("Enter a number [1, 2, 3]: ");
-        int selection = keyboardReader.nextInt();
+    /**
+     * Load customers from file. It's in the name.
+     */
+    public static void loadCustomersFromFile() {
+        System.out.println();
+        customers = new ArrayList<Customer>();
+        sortedCustomers = new ArrayList<Customer>();
+        setFile();
+        hasLoadedData = true;
+        isSorted = false;
+    }
+
+    /**
+     * Print loaded customer list. It's in the name.
+     */
+    public static void printLoadedList() {
+        if (checkDataLoad()) {
+            printCustomerList(customers);
+        }
+    }
+
+    /**
+     * Sort customers. It's in the name.
+     *
+     * @param sortType Sort by `name` or `id`.
+     */
+    public static void sortCustomers(String sortType) {
+        if (checkDataLoad()) {
+            if (sortType.equals("name")) {
+                sort1(customers);
+            } else if (sortType.equals("id")) {
+                sort2(customers);
+            }
+            System.out.println("Sorting is done!");
+            System.out.println();
+            isSorted = true;
+        }
+    }
+
+    /**
+     * Print sorted list. It's in the name.
+     */
+    public static void printSortedList() {
+        if (checkDataLoad()) {
+            if (isSorted) {
+                printCustomerList(sortedCustomers);
+            } else {
+                System.out.println("Nothing is sorted yet!");
+                System.out.println();
+            }
+        }
+    }
+
+    /**
+     * Check `hasLoadedData` and print message.
+     *
+     * @return boolean hasLoadedData
+     */
+    public static boolean checkDataLoad() {
+        if (!hasLoadedData) {
+            System.out.println("No data has been loaded yet!");
+            System.out.println();
+        }
+
+        return hasLoadedData;
+    }
+
+    /**
+     * Quicksort customer list by name.
+     *
+     * @param customerList list to sort
+     */
+    public static void sort1(List<Customer> customerList) {
+        // QuickSort sort
+        // customerList.sort((o1, o2) -> (int) (o1.getId() - o2.getId()));
+        // customerList.sort(Comparator.comparing(Customer::getId));
+        // customerList.sort(Comparator.comparing(Customer::getName));
+        sortedCustomers = customerList.stream()
+            .sorted(Comparator.comparing(Customer::getName))
+            .collect(Collectors.toList());
+    }
+
+    /**
+     * Quicksort customer list by id.
+     *
+     * @param customerList list to sort
+     */
+    public static void sort2(List<Customer> customerList) {
+        sortedCustomers = customerList.stream()
+            .sorted(Comparator.comparing(Customer::getId))
+            .collect(Collectors.toList());
+    }
+
+    /**
+     * Set the file and add customers to class field.
+     */
+    public static void setFile() {
+        Scanner input = keyboardReader;
+
+        System.out.print("Enter the name of the file: ");
+        String fileName = input.next();
+        addCustomersFromFile(fileName, customers);
+
+        System.out.println("Loading from the file is done!");
+        System.out.println();
+    }
+
+    /**
+     * Print customer list.
+     *
+     * @param listToPrint customer list to print
+     */
+    public static void printCustomerList(List<Customer> listToPrint) {
+        Scanner input = keyboardReader;
+
+        String selection = "";
+        int index = listToPrint.size();
+        int numbersToDisplay = 10;
+        int displayCount = 0;
+
+        boolean validInput = false;
+        System.out.println();
+        System.out.println("**** Printing the list ****");
+        int repeatIndex = displayCount;
+        while (!validInput && repeatIndex < listToPrint.size()) {
+            while (
+                repeatIndex < listToPrint.size()
+                    && repeatIndex < numbersToDisplay + displayCount
+            ) {
+                System.out.printf("%s. %s\n",
+                    repeatIndex + 1, listToPrint.get(repeatIndex).toString());
+                repeatIndex++;
+            }
+
+            displayCount = displayCount + 10;
+            if (repeatIndex < listToPrint.size()) {
+                System.out.print(
+                    "Enter something to continue/enter s to stop: ");
+                selection = input.next();
+                validInput =
+                    selection.equals("s")
+                        || (repeatIndex == listToPrint.size() - 1);
+            } else {
+                validInput = true;
+            }
+
+            index--;
+        }
+        System.out.println();
+    }
+
+    /**
+     * Create a menu selection.
+     *
+     * @param options list of options to select from
+     * @return menu selection int
+     */
+    public static int menuSelection(String[] options) {
+        Scanner input = keyboardReader;
+
+        int selection = -1;
+        boolean validInput = false;
+        // System.out.println("Choose from these choices");
+        // System.out.println("-------------------------\n");
+
+        for (int index = 1; index < options.length + 1; index++) {
+            System.out.println(index + ". " + options[index - 1]);
+        }
+        System.out.printf("Enter a number[1-%s]: ", options.length);
+        while (!validInput) {
+            selection = input.nextInt();
+            validInput = (selection > 0 && selection < options.length + 1);
+        }
         return selection;
     }
+
+    /**
+     * Create array of customers from a file with customer information.
+     *
+     * @param filename  filename to read from
+     * @param customers array of customers to add too
+     * @return array of customers
+     */
+    public static Object addCustomersFromFile(
+        String filename, List<Customer> customers
+    ) {
+        try {
+            File fileObject = new File(filename);
+            Scanner fileReader = new Scanner(fileObject);
+            while (fileReader.hasNext()) {
+                String customerName = fileReader.next();
+                long customerId = fileReader.nextLong();
+                Customer customer = new Customer(customerName, customerId);
+                customers.add(customer);
+            }
+
+            fileReader.close();
+            return customers;
+        } catch (FileNotFoundException fileNotFoundException) {
+            return new Customer[0];
+        }
+    }
 }
+```
